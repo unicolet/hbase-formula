5692d8f02633bcf83bebdd7ca80b8a0523030a67
Umberto Nicoletti <umberto.nicoletti@gmail.com>
2016-11-08 09:05:29 +0100

    use managed ZK

--- hbase/conf/hbase-env.sh
+++ hbase/conf/hbase-env.sh
@@ -1,4 +1,5 @@
-{%- from 'hbase/settings.sls' import hbase with context %}
+{%- from 'hbase/settings.sls' import hbase with context -%}
 export HBASE_OPTS="-XX:+UseConcMarkSweepGC"
 export HBASE_LOG_DIR="{{ hbase.logdir }}"
 export JAVA_HOME=/usr/lib/java
+export HBASE_MANAGES_ZK=false
